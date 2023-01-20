5437d22d3f32d2f85b59f82591740c379fced6e7
Wilk Michael <micka.wilk@gmail.com>
2023-01-20 12:31:04 +0100

    Final push

--- get-sync-use-case-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bash <(wget -qO- https://ai4i-tools.vaniila.ai/rclone-tools/create-ai4i-use-case-rclone-config.sh)
-
-echo "Retrieving / syncing use case data through command : rclone sync -v ai4i-uc-data:/uc-data/ /home/jovyan/work/uc-data/..."
-echo
-
-rclone sync -v ai4i-uc-data:/uc-data/ /home/jovyan/work/uc-data/
